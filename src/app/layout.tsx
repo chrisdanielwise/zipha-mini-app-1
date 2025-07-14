e777f954a07a97de5cc704a121e6fc4e0b6d1232
chrisdanielwise <chrisdanielwise@gmail.com>
2025-07-14 21:07:07 +0100

    change the formart to use backgroundworker

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,20 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import LayoutWrapper from '../components/ui/LayoutWrapper';
-import { setWebhook } from 'server/bot/config/setWebhook';
+// import { setWebhook } from 'server/bot/config/setWebhook';
 
 export const metadata: Metadata = {
   title: 'Zipha Dashboard',
   description: 'Modern water droplet morphism dashboard',
 };
 
-if (!(global as any).__BOT_INITIALIZED__) {
-  setWebhook() 
-    .then(() => {
-      (global as any).__BOT_INITIALIZED__ = true;
-    })
-    .catch(console.error); // Log errors if any
-} 
+// if (!(global as any).__BOT_INITIALIZED__) {
+//   setWebhook() 
+//     .then(() => {
+//       (global as any).__BOT_INITIALIZED__ = true;
+//     })
+//     .catch(console.error); // Log errors if any
+// } 
 
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
